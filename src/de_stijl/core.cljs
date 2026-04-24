857bec2fcfdeb1f0a623c7f1f92640d9400d0fbc
Yetkin Ozturk <yetkin85@gmail.com>
2026-04-24 10:27:01 +0200

    deploy

--- src/de_stijl/core.cljs
+++
@@ -1,8 +0,0 @@
-(ns de-stijl.core
-  (:require [de-stijl.ui :as ui]))
-
-;; ─── Entry point ─────────────────────────────────────────────────────────────
-
-(defn init! []
-  (js/console.log "De Stijl — init")
-  (ui/mount!))
